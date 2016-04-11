a528f6f774f50eba8700354b89c89edacf0590c2
Masayuki Takahashi <masayuki038@gmail.com>
2016-04-12 03:58:16 +0900

    Define `Node` And `State`

---
+++ src/main/scala/net/wrap_trap/goju/Bloom.scala
@@ -0,0 +1,8 @@
+package net.wrap_trap.goju
+
+/**
+  * Created by masayuki on 2016/04/12.
+  */
+class Bloom {
+
+}
--- src/main/scala/net/wrap_trap/goju/Writer.scala
+++ src/main/scala/net/wrap_trap/goju/Writer.scala
@@ -1,5 +1,9 @@
 package net.wrap_trap.goju
 
+import scala.io.Source
+
+import net.wrap_trap.goju.Constants._
+
 /**
   * goju-to: HanoiDB(LSM-trees (Log-Structured Merge Trees) Indexed Storage) clone
 
@@ -10,5 +14,16 @@ package net.wrap_trap.goju
   */
 class Writer {
 
-
 }
+
+case class Node(level: Int, members: Array[(Key, ExpValue)], size: Int = 0)
+case class State(indexFile: Source,
+                  indexFilePos: Int,
+                  lastNodePos: Long,
+                  lastNodeSize: Int,
+                  nodes: Array[Node],
+                  name: String,
+                  bloom: Bloom,
+                  opts: Array[Any],
+                  valueCount: Int = 0,
+                  tombstoneCount: Int = 0)
