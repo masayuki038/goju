b8368a3700314536fe50a4a29e2bfb322060058e
Masayuki Takahashi <masayuki038@gmail.com>
2016-05-12 04:19:28 +0900

    Add serializer for value

--- src/main/scala/net/wrap_trap/goju/Serializer.scala
+++ src/main/scala/net/wrap_trap/goju/Serializer.scala
@@ -1,7 +1,10 @@
 package net.wrap_trap.goju
 
 import java.io.ByteArrayOutputStream
+import java.math.BigDecimal
+import java.math.BigInteger
 
+import msgpack4z.MsgOutBuffer
 import net.wrap_trap.goju.Constants._
 import net.wrap_trap.goju.Helper._
 import net.wrap_trap.goju.element.{KeyValue, Element}
@@ -15,21 +18,13 @@ import net.wrap_trap.goju.element.{KeyValue, Element}
   * http://opensource.org/licenses/mit-license.php
   */
 object Serializer {
-
-}
-
-abstract class Serializer {
-  def serialize(entry: Element): Array[Byte]
-}
-
-class KeyValueSerializer extends Serializer {
-  override def serialize(entry: Element): Array[Byte] = {
+  def serialize(entry: Element): Array[Byte] = {
     entry match {
-      case kv: KeyValue => toBytes(kv)
+      case kv: KeyValue => serialize(kv)
     }
   }
 
-  def toBytes(kv: KeyValue): Array[Byte] = {
+  private def serialize(kv: KeyValue): Array[Byte] = {
     val baos = new ByteArrayOutputStream
     using(new ElementOutputStream((baos))) { eos =>
       kv.timestamp match {
@@ -40,8 +35,26 @@ class KeyValueSerializer extends Serializer {
         case _ => eos.writeByte(TAG_KV_DATA)
       }
       eos.writeInt(kv.key.length)
-      //eos.write(kv.value.to)
+      eos.write(serializeValue(kv.value))
       baos.toByteArray
     }
+  }
+
+  private def serializeValue(value: Any): Array[Byte] = {
+    val buf = MsgOutBuffer.create
+    value match {
+      case s: String => buf.packString(s)
+      case i: Int => buf.packInt(i)
+      case l: Long => buf.packLong(l)
+      case d: Double => buf.packDouble(d)
+      case f: Float => buf.packFloat(f)
+      case bi: BigInteger => buf.packBigInteger(bi)
+      case b: Byte => buf.packByte(b)
+      case ba: Array[Byte] => buf.packBinary(ba)
+      case bo: Boolean => buf.packBoolean(bo)
+      case unsupported =>
+        throw new IllegalArgumentException("Unsupported type of value: " + unsupported)
+    }
+    buf.result()
   }
 }
