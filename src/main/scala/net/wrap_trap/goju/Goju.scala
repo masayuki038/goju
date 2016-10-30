a38acc7a7c2f2a4a7ec5db1f0295a25445357447
Masayuki Takahashi <masayuki038@gmail.com>
2016-10-30 21:16:22 +0900

    Add Goju

---
+++ src/main/scala/net/wrap_trap/goju/Goju.scala
@@ -0,0 +1,71 @@
+package net.wrap_trap.goju
+
+import akka.actor._
+import akka.util.Timeout
+import net.wrap_trap.goju.Constants.Value
+import scala.concurrent.duration._
+
+/**
+  * goju: HanoiDB(LSM-trees (Log-Structured Merge Trees) Indexed Storage) clone
+
+  * Copyright (c) 2016 Masayuki Takahashi
+
+  * This software is released under the MIT License.
+  * http://opensource.org/licenses/mit-license.php
+  */
+object Goju extends PlainRpc {
+  val callTimeout = Settings.getSettings().getInt("goju.call_timeout", 300)
+  implicit val timeout = Timeout(callTimeout seconds)
+
+  def open(dirPath: String): Unit = {
+    new Goju(dirPath)
+  }
+
+  def close(ref: ActorRef): Unit = {
+    try {
+      call(ref, Close)
+    } catch {
+      case ignore => {}
+    }
+  }
+
+  def destroy(ref: ActorRef): Unit = {
+    try {
+      call(ref, Destroy)
+    } catch {
+      case ignore => {}
+    }
+  }
+
+  def get(ref: ActorRef, key: Array[Byte]): Value = {
+    call(ref, (Get, key))
+  }
+
+  def lookup(ref: ActorRef, key: Array[Byte]): Value = {
+    get(ref, key)
+  }
+
+  def delete(ref: ActorRef, key: Array[Byte]): Unit = {
+    call(ref, (Delete, key))
+  }
+
+  def put(ref: ActorRef, key: Array[Byte], value: Value): Unit = {
+    call(ref, (Put, key, value))
+  }
+
+  def transact(ref: ActorRef, transactionSpecs: List[(TransactionOp, Any)]): Unit = {
+    call(ref, (Transact, transactionSpecs))
+  }
+
+//  def fold(ref: ActorRef, func: (Array[Byte], Value, (Int, List[Value])) => (Int, List[Value]), acc0: (Int, List[Value])): List[Value] = {
+//    foldRange(ref, func, acc0, KeyRange())
+//  }
+}
+
+class Goju(val dirPath: String) {
+
+}
+
+sealed abstract class GojuOp
+case object Get extends GojuOp
+case object Transact extends GojuOp
