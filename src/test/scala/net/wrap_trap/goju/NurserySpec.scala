95ca5b1776f5a535541a1762d672fb82f07e68a4
Masayuki Takahashi <masayuki038@gmail.com>
2016-11-08 00:28:18 +0900

    Fix to fail to delete a log file in Nursery.recover

--- src/test/scala/net/wrap_trap/goju/NurserySpec.scala
+++ src/test/scala/net/wrap_trap/goju/NurserySpec.scala
@@ -2,10 +2,12 @@ package net.wrap_trap.goju
 
 import java.io.File
 
-import akka.actor.{ActorSystem, ActorRef}
+import akka.actor.{Actor, ActorSystem, ActorRef}
 import akka.testkit.{TestKit, TestActorRef}
+import com.typesafe.scalalogging.Logger
 import net.wrap_trap.goju.element.KeyValue
-import org.scalatest.{FlatSpecLike, BeforeAndAfter, Matchers, FlatSpec}
+import org.scalatest.{FlatSpecLike, BeforeAndAfter, Matchers}
+import org.slf4j.LoggerFactory
 
 /**
   * goju: HanoiDB(LSM-trees (Log-Structured Merge Trees) Indexed Storage) clone
@@ -17,28 +19,32 @@ import org.scalatest.{FlatSpecLike, BeforeAndAfter, Matchers, FlatSpec}
   */
 class NurserySpec extends TestKit(ActorSystem("test"))
  with FlatSpecLike with Matchers with BeforeAndAfter {
+  val log = Logger(LoggerFactory.getLogger(Nursery.getClass))
 
   after {
-    new File("./nursery.log").delete
+    val deleted = new File("./" +  Nursery.DATA_FILENAME).delete
+    log.info("Nursery's data has deleted: " + deleted)
   }
 
   "newNursery" should "return new Nursery" in {
     val nursery = Nursery.newNursery(".", 1, 2)
     nursery.isInstanceOf[Nursery] should be(true)
+    nursery.destroy()
   }
 
   "newNursery" should "create log file" in {
     val nursery = Nursery.newNursery(".", 1, 2)
     new File("./nursery.log").exists should be(true)
+    nursery.destroy()
   }
 
   def newNursery(testCode: (Nursery, ActorRef) => Any) {
-    testCode(Nursery.newNursery(".", 1, 2), TestActorRef[PlainRpcActor])
+    testCode(Nursery.newNursery(".", 1, 2), TestActorRef[LevelSutbForRecover])
   }
 
   def twoInNursery(testCode: (Nursery, ActorRef) => Any) {
     val nursery = Nursery.newNursery(".", 1, 2)
-    val top = TestActorRef[PlainRpcActor]
+    val top = TestActorRef[LevelSutbForRecover]
     Nursery.add(Utils.toBytes("foo"), "bar", 5, nursery, top)
     Nursery.add(Utils.toBytes("hoge"), "hogehoge", 10, nursery, top)
     testCode(nursery, top)
@@ -60,6 +66,7 @@ class NurserySpec extends TestKit(ActorSystem("test"))
     nursery.tree.containsKey(key2) should be(true)
     nursery.tree.get(key2).isInstanceOf[KeyValue] should be(true)
     nursery.tree.get(key2).asInstanceOf[KeyValue].value should be("hogehoge")
+    nursery.destroy()
   }
 
   // TODO Add Nurseary.add should KeyValue with expire time when implement Nursery.get
@@ -73,23 +80,18 @@ class NurserySpec extends TestKit(ActorSystem("test"))
 //  }
 
   "Nursery.recover" should "be recoverd two elements" in twoInNursery { (nursery, top) =>
-    nursery.logger.close
-    val key1 = Key(Utils.toBytes("foo"))
-    val key2 = Key( Utils.toBytes("hoge"))
-
+    nursery.logger.close()
     val newNursery = Nursery.recover(".", top, 1, 2)
 
     val reader = RandomReader.open(newNursery.dirPath + java.io.File.separator + Nursery.DATA_FILENAME)
     reader.lookup(Utils.toBytes("foo")) should be(Option("bar"))
     reader.lookup(Utils.toBytes("hoge")) should be(Option("hogehoge"))
-    reader.destroy
+    reader.destroy()
+    newNursery.destroy()
   }
 
   "Nursery.recover" should "be recoverd two elements with expire time" in twoInNursery { (nursery, top) =>
-    nursery.logger.close
-    val key1 = Key(Utils.toBytes("foo"))
-    val key2 = Key( Utils.toBytes("hoge"))
-
+    nursery.logger.close()
     val newNursery = Nursery.recover(".", top, 1, 2)
     val reader = RandomReader.open(newNursery.dirPath + java.io.File.separator + Nursery.DATA_FILENAME)
 
@@ -101,6 +103,19 @@ class NurserySpec extends TestKit(ActorSystem("test"))
     reader.lookup(Utils.toBytes("foo")) should be(None)
     reader.lookup(Utils.toBytes("hoge")) should be(None)
 
-    reader.destroy
+    reader.destroy()
+    newNursery.destroy()
+  }
+}
+
+class LevelSutbForRecover extends Actor with PlainRpc {
+
+  def receive = {
+    case (PlainRpcProtocol.call, (Inject, fileName: String)) => {
+      sendReply(sender(), Ok)
+    }
+    case (PlainRpcProtocol.call, (BeginIncrementalMerge, stepSize: Int)) => {
+      sendReply(sender(), true)
+    }
   }
 }
