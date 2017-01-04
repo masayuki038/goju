06e8484e988de8e5010d6d3c6d17a3918a3183ad
masayuki038 <masayuki038@gmail.com>
2017-01-05 00:57:56 +0900

    Fix FoldSpec bug

--- src/test/scala/net/wrap_trap/goju/FoldSpec.scala
+++ src/test/scala/net/wrap_trap/goju/FoldSpec.scala
@@ -30,7 +30,7 @@ class FoldSpec extends TestKit(ActorSystem("goju"))
 
   "To fold 1024 entries" should "return all entries in levels" in {
     val goju = Goju.open("test-data")
-    (1 to 1024).foreach(i => goju.put(Utils.toBytes("key" + i), Utils.toBytes("value" + i)))
+    (1 to 1024).foreach(i => goju.put(Utils.toBytes("key" + i), "value" + i))
     Thread.sleep(5000L)
 
     var ret = goju.foldRange((k, v, acc) => {
@@ -38,11 +38,14 @@ class FoldSpec extends TestKit(ActorSystem("goju"))
       (count + 1, v :: list)
     }, (0, List.empty[Value]),
       KeyRange(Key(Utils.toBytes("key1")), true, Option(Key(Utils.toBytes("key3"))), false, Integer.MAX_VALUE))
+
     val set12 = ret.toSet
-    (1 to 2).foreach(i => set12.contains("value" + i) should be(true))
-    (10 to 29).foreach(i => set12.contains("value" + i) should be(true))
-    (100 to 299).foreach(i => set12.contains("value" + i) should be(true))
-    (1000 to 1024).foreach(i => set12.contains("value" + i) should be(true))
+    ret.foreach(p => println(p))
+
+    (1 to 2).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
+    (10 to 29).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
+    (100 to 299).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
+    (1000 to 1024).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
 
     ret = goju.foldRange((k, v, acc) => {
       val (count, list) = acc
@@ -50,9 +53,9 @@ class FoldSpec extends TestKit(ActorSystem("goju"))
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key3")), true, Option(Key(Utils.toBytes("key5"))), false, Integer.MAX_VALUE))
     val set34 = ret.toSet
-    (3 to 4).foreach(i => set34.contains("value" + i) should be(true))
-    (30 to 49).foreach(i => set34.contains("value" + i) should be(true))
-    (300 to 499).foreach(i => set34.contains("value" + i) should be(true))
+    (3 to 4).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
+    (30 to 49).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
+    (300 to 499).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
 
     ret= goju.foldRange((k, v, acc) => {
       val (count, list) = acc
@@ -60,9 +63,9 @@ class FoldSpec extends TestKit(ActorSystem("goju"))
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key5")), true, Option(Key(Utils.toBytes("key7"))), false, Integer.MAX_VALUE))
     val set56 = ret.toSet
-    (5 to 6).foreach(i => set56.contains("value" + i) should be(true))
-    (50 to 69).foreach(i => set56.contains("value" + i) should be(true))
-    (500 to 699).foreach(i => set56.contains("value" + i) should be(true))
+    (5 to 6).foreach(i => withClue("value" + i){set56("value" + i) should be(true)})
+    (50 to 69).foreach(i => withClue("value" + i){set56("value" + i) should be(true)})
+    (500 to 699).foreach(i => withClue("value" + i){set56("value" + i) should be(true)})
 
     ret = goju.foldRange((k, v, acc) => {
       val (count, list) = acc
@@ -70,18 +73,19 @@ class FoldSpec extends TestKit(ActorSystem("goju"))
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key7")), true, Option(Key(Utils.toBytes("key9"))), false, Integer.MAX_VALUE))
     val set78 = ret.toSet
-    (7 to 8).foreach(i => set78.contains("value" + i) should be(true))
-    (70 to 89).foreach(i => set78.contains("value" + i) should be(true))
-    (700 to 899).foreach(i => set78.contains("value" + i) should be(true))
+    (7 to 8).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
+    (70 to 89).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
+    (700 to 899).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
 
     ret = goju.foldRange((k, v, acc) => {
       val (count, list) = acc
       (count + 1, v :: list)
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key9")), true, None, false, Integer.MAX_VALUE))
-    set78.contains("value9") should be(true)
-    (90 to 99).foreach(i => set78.contains("value" + i) should be(true))
-    (900 to 999).foreach(i => set78.contains("value" + i) should be(true))
+    val set9 = ret.toSet
+    set9.contains("value9") should be(true)
+    (90 to 99).foreach(i => withClue("value" + i){set9("value" + i) should be(true)})
+    (900 to 999).foreach(i => withClue("value" + i){set9("value" + i) should be(true)})
     goju.destroy()
   }
 
