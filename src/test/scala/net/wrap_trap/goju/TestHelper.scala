64125420733c153ab83f06ce1f4de194d1758ae5
Masayuki Takahashi <masayuki038@gmail.com>
2017-01-08 12:40:25 +0900

    Add log for failing delete directory in TravisCI

--- src/test/scala/net/wrap_trap/goju/TestHelper.scala
+++ src/test/scala/net/wrap_trap/goju/TestHelper.scala
@@ -2,6 +2,8 @@ package net.wrap_trap.goju
 
 import java.io.File
 
+import akka.event.{LogSource, Logging}
+
 /**
   * goju: HanoiDB(LSM-trees (Log-Structured Merge Trees) Indexed Storage) clone
 
@@ -11,11 +13,15 @@ import java.io.File
   * http://opensource.org/licenses/mit-license.php
   */
 object TestHelper {
+  implicit val logSource: LogSource[AnyRef] = new GojuLogSource()
+  val log = Logging(Utils.getActorSystem, this)
+
   def deleteDirectory(file: File): Unit = {
     if(file.isDirectory) {
       Option(file.listFiles).map(_.toList).getOrElse(Nil).foreach(deleteDirectory(_))
     }
     if(file.exists && !file.delete) {
+      file.listFiles.foreach(f => f.getAbsolutePath)
       throw new IllegalStateException("Failed to delete: " + file.getName)
     }
   }
