4a203ca6d034f424c5df41410397ee93cca4c99d
Masayuki Takahashi <masayuki038@gmail.com>
2017-01-15 13:35:28 +0900

    Add debug log for rangeFold in FoldSpec

--- src/test/scala/net/wrap_trap/goju/FoldSpec.scala
+++ src/test/scala/net/wrap_trap/goju/FoldSpec.scala
@@ -1,9 +1,10 @@
 package net.wrap_trap.goju
 
-import java.io.{RandomAccessFile, FileWriter, File}
+import java.io.{File, FileWriter, RandomAccessFile}
 import java.nio.file.Files
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.testkit.TestKit
 import net.wrap_trap.goju.Constants.Value
 import net.wrap_trap.goju.Helper._
@@ -22,6 +23,8 @@ class FoldSpec extends FlatSpecLike
   with BeforeAndAfter
   with PlainRpcClient {
 
+  val log = Logging(Utils.getActorSystem, this)
+
   "To fold 1024 entries" should "return all entries in levels" in {
     TestHelper.deleteDirectory(new File("test-fold1"))
     val goju = Goju.open("test-fold1")
@@ -34,6 +37,8 @@ class FoldSpec extends FlatSpecLike
     }, (0, List.empty[Value]),
       KeyRange(Key(Utils.toBytes("key1")), true, Option(Key(Utils.toBytes("key3"))), false, Integer.MAX_VALUE))
     val set12 = ret.toSet
+    log.debug("content set12: " + ret.mkString(", "))
+    ret.foreach(p => println(p))
     (1 to 2).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
     (10 to 29).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
     (100 to 299).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
@@ -45,6 +50,8 @@ class FoldSpec extends FlatSpecLike
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key3")), true, Option(Key(Utils.toBytes("key5"))), false, Integer.MAX_VALUE))
     val set34 = ret.toSet
+    log.debug("content set34: " + ret.mkString(", "))
+    ret.foreach(p => println(p))
     (3 to 4).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
     (30 to 49).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
     (300 to 499).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
@@ -55,6 +62,7 @@ class FoldSpec extends FlatSpecLike
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key5")), true, Option(Key(Utils.toBytes("key7"))), false, Integer.MAX_VALUE))
     val set56 = ret.toSet
+    log.debug("content set56: " + ret.mkString(", "))
     (5 to 6).foreach(i => withClue("value" + i){set56("value" + i) should be(true)})
     (50 to 69).foreach(i => withClue("value" + i){set56("value" + i) should be(true)})
     (500 to 699).foreach(i => withClue("value" + i){set56("value" + i) should be(true)})
@@ -65,6 +73,8 @@ class FoldSpec extends FlatSpecLike
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key7")), true, Option(Key(Utils.toBytes("key9"))), false, Integer.MAX_VALUE))
     val set78 = ret.toSet
+    log.debug("content set78: " + ret.mkString(", "))
+    ret.foreach(p => println(p))
     (7 to 8).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
     (70 to 89).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
     (700 to 899).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
@@ -75,6 +85,8 @@ class FoldSpec extends FlatSpecLike
     }, (0, ret),
       KeyRange(Key(Utils.toBytes("key9")), true, None, false, Integer.MAX_VALUE))
     val set9 = ret.toSet
+    log.debug("content set9:" + ret.mkString(", "))
+    ret.foreach(p => println(p))
     set9.contains("value9") should be(true)
     (90 to 99).foreach(i => withClue("value" + i){set9("value" + i) should be(true)})
     (900 to 999).foreach(i => withClue("value" + i){set9("value" + i) should be(true)})
