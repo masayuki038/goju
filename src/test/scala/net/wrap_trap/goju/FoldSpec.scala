55d42df68955bc8e295cdf265c04f569c721ea6d
Masayuki Takahashi <masayuki038@gmail.com>
2017-01-15 19:54:09 +0900

    Change acc0 to empty list in FoldSpec to be able to check rangeFold results clearly

--- src/test/scala/net/wrap_trap/goju/FoldSpec.scala
+++ src/test/scala/net/wrap_trap/goju/FoldSpec.scala
@@ -38,7 +38,6 @@ class FoldSpec extends FlatSpecLike
       KeyRange(Key(Utils.toBytes("key1")), true, Option(Key(Utils.toBytes("key3"))), false, Integer.MAX_VALUE))
     val set12 = ret.toSet
     log.debug("content set12: " + ret.mkString(", "))
-    ret.foreach(p => println(p))
     (1 to 2).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
     (10 to 29).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
     (100 to 299).foreach(i => withClue("value" + i){set12("value" + i) should be(true)})
@@ -47,11 +46,10 @@ class FoldSpec extends FlatSpecLike
     ret = goju.foldRange((k, v, acc) => {
       val (count, list) = acc
       (count + 1, v :: list)
-    }, (0, ret),
+    }, (0, List.empty[Value]),
       KeyRange(Key(Utils.toBytes("key3")), true, Option(Key(Utils.toBytes("key5"))), false, Integer.MAX_VALUE))
     val set34 = ret.toSet
     log.debug("content set34: " + ret.mkString(", "))
-    ret.foreach(p => println(p))
     (3 to 4).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
     (30 to 49).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
     (300 to 499).foreach(i => withClue("value" + i){set34("value" + i) should be(true)})
@@ -59,7 +57,7 @@ class FoldSpec extends FlatSpecLike
     ret= goju.foldRange((k, v, acc) => {
       val (count, list) = acc
       (count + 1, v :: list)
-    }, (0, ret),
+    }, (0, List.empty[Value]),
       KeyRange(Key(Utils.toBytes("key5")), true, Option(Key(Utils.toBytes("key7"))), false, Integer.MAX_VALUE))
     val set56 = ret.toSet
     log.debug("content set56: " + ret.mkString(", "))
@@ -70,11 +68,10 @@ class FoldSpec extends FlatSpecLike
     ret = goju.foldRange((k, v, acc) => {
       val (count, list) = acc
       (count + 1, v :: list)
-    }, (0, ret),
+    }, (0, List.empty[Value]),
       KeyRange(Key(Utils.toBytes("key7")), true, Option(Key(Utils.toBytes("key9"))), false, Integer.MAX_VALUE))
     val set78 = ret.toSet
     log.debug("content set78: " + ret.mkString(", "))
-    ret.foreach(p => println(p))
     (7 to 8).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
     (70 to 89).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
     (700 to 899).foreach(i => withClue("value" + i){set78("value" + i) should be(true)})
@@ -82,14 +79,14 @@ class FoldSpec extends FlatSpecLike
     ret = goju.foldRange((k, v, acc) => {
       val (count, list) = acc
       (count + 1, v :: list)
-    }, (0, ret),
+    }, (0, List.empty[Value]),
       KeyRange(Key(Utils.toBytes("key9")), true, None, false, Integer.MAX_VALUE))
     val set9 = ret.toSet
     log.debug("content set9:" + ret.mkString(", "))
-    ret.foreach(p => println(p))
     set9.contains("value9") should be(true)
     (90 to 99).foreach(i => withClue("value" + i){set9("value" + i) should be(true)})
     (900 to 999).foreach(i => withClue("value" + i){set9("value" + i) should be(true)})
+    Thread.sleep(10000L)
     goju.destroy()
   }
 }
